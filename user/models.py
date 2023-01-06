5a566a7cf48abc31be251060f39052a6726014d7
Milan Biranwar <milanbiranwar06@gmail.com>
2023-01-06 20:49:48 +0530

    [Milan] Added . viewset code

--- user/models.py
+++ user/models.py
@@ -7,3 +7,8 @@ class User(models.Model):
     user_name = models.CharField(max_length=300)
     date = models.DateTimeField(auto_now_add=True)
 
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    roll_num = models.IntegerField()
+    city = models.CharField(max_length=100)
